7ad3aaf0743546dcfe51b8b9cf66aa9a100989aa
Casey Calkins <c.angelcalkins@outlook.com>
2019-06-14 12:04:18 -0500

    Creating game login

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const container = document.getElementById('container');
-const gameBoard = document.createElement('div');
-gameBoard.className = 'game-board';
+// let gameBoard = document.createElement('div');
+// gameBoard.setAttribute('class', 'game-board');
+
+
+let userInput = prompt('Enter a number between 10-20: ');
+if (isNaN(userInput)) {
+    alert('Numbers only.');
+    location.reload();
+} else {
+    console.log('A number');
+}
+
+
+for (let index = 1; index <= userInput; index++) {25
+    let gameBoard = document.createElement('div');
+    gameBoard.setAttribute('class', 'game-board');
+    container.appendChild(gameBoard);
+}
+
+
 
